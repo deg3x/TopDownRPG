93a3182a323729e7d0c3b07cdedf836674376e7a
Konstantinos Benos <mpenos.ks@gmail.com>
2022-05-24 01:58:05 +0300

    Make item description popup fit its content size

--- Assets/Scripts/UI/UIItemDescriptionPopup.cs
+++ Assets/Scripts/UI/UIItemDescriptionPopup.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UIItemDescriptionPopup : MonoBehaviour
 {
@@ -11,11 +12,14 @@ public class UIItemDescriptionPopup : MonoBehaviour
     private Text description;
 
     private RectTransform rect;
+    private Image image;
     private Vector3 targetWorldPos;
     private bool isActive;
 
-    private void Start()
+    private void Awake()
     {
+        isActive = false;
+        image = GetComponent<Image>();
         rect = GetComponent<RectTransform>();
     }
 
@@ -31,23 +35,30 @@ public class UIItemDescriptionPopup : MonoBehaviour
 
     public void ActivateDescriptionPopup(Vector3 itemWorldPosition, string itemTitle, string itemDescription)
     {
-        gameObject.SetActive(true);
         title.text = itemTitle;
         description.text = itemDescription;
-
         targetWorldPos = itemWorldPosition;
 
-        isActive = true;
+        StartCoroutine(UpdatePopupSize());
     }
 
     public void DeactivateDesctiptionPopup()
     {
-        gameObject.SetActive(false);
         isActive = false;
+        rect.position = Vector3.down * 2000.0f;     // Dirty way to hide the item popup, but solves game object activation problems
     }
 
     private Vector2 CalculatePopupScreenPosition(Vector3 itemWorldPosition)
     {
         return Camera.main.WorldToScreenPoint(itemWorldPosition) + Vector3.up * 50.0f;
+    }
+
+    private IEnumerator UpdatePopupSize()
+    {
+        yield return null;
+
+        rect.sizeDelta = new Vector2(rect.sizeDelta.x, title.rectTransform.sizeDelta.y + description.rectTransform.sizeDelta.y + 10.0f);
+
+        isActive = true;
     }
 }
